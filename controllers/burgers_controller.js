184d44c3095b96bd92bc966964ac431fce046b11
Meenal <meenalalagappan89@gmail.com>
2019-02-02 23:40:24 -0800

    initial commit

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,66 @@
+var express = require("express");
+var router = express.Router();
+var db = require("../models");
+var sequelize= require("sequelize");
+
+// get route -> index
+router.get("/", function(req, res) {
+  res.redirect("/burgers");
+});
+
+router.get("/burgers", function(req, res) {
+  // console.log("burgers fired");
+  
+  // express callback response by calling burger.selectAllBurger
+  db.Burger.findAll({
+    include: db.Customer, 
+    // where: {Customer.id :req.CustomerId}, 
+    order: sequelize.col("burger_name")
+  }).then(function(data) {
+    // Wrapping the array of returned burgers in a object so it can be referenced inside our handlebars
+    var burger_data = data.map(x => x.dataValues);
+    var hbsObject = 
+    { Burger: burger_data};
+    res.render("index", hbsObject);
+  });
+});
+
+// post route -> back to index
+router.post("/burgers/create", function(req, res) {
+  // takes the request object using it as input for buger.addBurger
+  db.Burger.create(req.body).then(function(result) {
+    // wrapper for orm.js that using MySQL insert callback will return a log to console,
+    // render back to index with handle
+    // console.log(result);
+    res.redirect("/");
+  });
+});
+
+router.post("/burgers/customer", function(req, res){
+  db.Customer.create(req.body).then(function(result){
+    res.json(result);
+    console.log(result)
+    // res.redirect(`/burger/update/${result.id}`)
+  })
+})
+
+// put route -> back to index
+router.put("/burgers/update/:id", function(req, res) {
+  console.log(req.body);
+  db.Burger.update(
+    req.body,
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  ).then(function(result) {
+    // wrapper for orm.js that using MySQL update callback will return a log to console,
+    // render back to index with handle
+    console.log(`WORK WORK WORK  ${result}`);
+    // Send back response and let page reload from .done in Ajax
+    res.json("/");
+  });
+});
+
+module.exports = router;
---
+++ public/assets/js/script.js
@@ -0,0 +1,50 @@
+$(function() {
+    $(".change-devoured").on("submit", function(event) {
+      event.preventDefault();
+      var id = $(".btn-success").data("id");
+      var customerName = $(".customerName").val().trim();
+      console.log(customerName)
+      //Post a new customer then PUT onto burger
+      if (customerName){
+       $.ajax(`/burgers/customer`, {
+          type: "POST",
+          data: {
+            customer_name: customerName
+          }
+        }).then(function(res) {
+          var customerId = res.id;
+          $.ajax(`/burgers/update/${id}`, {
+            type: "PUT",
+            data: {
+              CustomerId: customerId,
+              devoured: 1
+            }   
+          }).then(function() {
+            console.log("customer added to burger");
+            location.reload();
+          });
+        });
+      }
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+      if ($("#enter_text").val().trim() != "") {
+            var newburger = {
+            burger_name: $("#enter_text").val().trim(),
+            devoured: $("[name=devoured]:checked").val()
+          };
+  
+          // Send the POST request.
+          $.ajax("/burgers/create", {
+            type: "POST",
+            data: newburger
+          }).then(function() {
+            console.log("created new burger");
+            // Reload the page to get the updated list
+            location.reload();
+          });
+        };
+     })
+    });
